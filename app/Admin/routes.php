8b539f84006d75e002fe764e72931dd408740c02
Your Name <liyonghzeng@2548722906@qq.com>
2019-04-25 13:10:39 +0000

    后台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,8 +9,8 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-    $router->get('/goods/list', GoodsController::class);
-
+//    $router->get('/goods/list', GoodsController::class);
+    Route::resource('/goods/list', GoodsController::class);
     $router->get('/', 'HomeController@index');
     $router->get('weixin/list', 'WeixinController@index');
 
